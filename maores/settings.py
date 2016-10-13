2b9fa6e8b7a1294234e943d0c96fa9ebdce70035
fernandodr <frcompsci@gmail.com>
2016-10-13 01:16:53 -0400

    fixed logging in functionality

--- maores/settings.py
+++ maores/settings.py
@@ -92,11 +92,19 @@ WSGI_APPLICATION = 'maores.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'famatresults',
+    'USER': ''
+    'PASSWORD': '',
+    'HOST': ''
 }
 
 
