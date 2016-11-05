19751a5ee17a8d35138fb45885d1e124f40dd62b
frtennis1 <frtennis1@gmail.com>
2016-11-05 07:58:47 -0400

    remove port name from db hostname

--- maores/settings.py
+++ maores/settings.py
@@ -114,7 +114,7 @@ if True:
             'NAME': 'famatviz',
             'USER': 'frivera',
             'PASSWORD': 'b-A3GYaO1S',
-            'HOST': 'famatviz.cy7xelq2fymc.us-west-2.rds.amazonaws.com:5432',
+            'HOST': 'famatviz.cy7xelq2fymc.us-west-2.rds.amazonaws.com',
             'PORT': '5432',
         }
     }
