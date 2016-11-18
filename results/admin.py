7531589556725d1e1cf1185947c8233968764d4f
fernandodr <frcompsci@gmail.com>
2016-11-17 19:13:40 -0500

    Modified model templates and added impressions.

--- results/admin.py
+++ results/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Mathlete)
+class MathleteAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'mao_id', 'school')
+
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'category')
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('competition', 'division')
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('test', 'number', 'answer', 'num_correct', 'num_blank', 'num_wrong')
+
+class TestPaperAdmin(admin.ModelAdmin):
+    list_display = ('test', 'place', 'mathlete', 'school')
+
+class QuestionAnswerAdmin(admin.ModelAdmin):
+    list_display = ('paper', 'question', 'givenanswer', 'points')
+
+class MathleteImpressionAdmin(admin.ModelAdmin):
+    list_display = ('mathlete', 'user', 'datetime')
+
+admin.site.register(Mathlete, MathleteAdmin)
 admin.site.register(School)
-admin.site.register(Competition)
-admin.site.register(Test)
-admin.site.register(Question)
-admin.site.register(TestPaper)
-admin.site.register(QuestionAnswer)
+admin.site.register(Competition, CompetitionAdmin)
+admin.site.register(Test, TestAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(TestPaper, TestPaperAdmin)
+admin.site.register(QuestionAnswer, QuestionAnswerAdmin)
 admin.site.register(Team)
+admin.site.register(MathleteImpression, MathleteImpressionAdmin)
