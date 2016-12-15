2ff5437e8f6723e640a41bdbdd84bb432923f1ea
fernandodrg <frcompsci@gmail.com>
2016-12-15 10:33:14 -0500

    Use production database.

--- maores/settings.py
+++ maores/settings.py
@@ -105,7 +105,7 @@ SOCIALACCOUNT_PROVIDERS = \
         { 'SCOPE': ['profile', 'email'],
           'AUTH_PARAMS': { 'access_type': 'online' } }}
 
-if False:
+if True:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
